07c9b55eaf523b8871a216cbf49a3d33c28e7206
Connor315 <gao315320130@gmail.com>
2024-12-10 12:56:18 -0500

    Store Channel name in local storage

--- frontend/src/lib.rs
+++ frontend/src/lib.rs
@@ -5,6 +5,7 @@ use wasm_bindgen_futures::spawn_local;
 use web_sys::{HtmlInputElement, Event}; 
 use yew::prelude::*;
 use gloo::utils::window;
+use gloo_storage::{Storage, LocalStorage};
 
 #[derive(Deserialize, Debug, Clone)]
 struct Channel {
@@ -250,12 +251,12 @@ fn channel_list() -> Html {
         }
     }, ());
 
-    let selected_channel = use_state(|| None::<i32>);
+    let selected_channel = use_state(|| None::<String>);
 
     let on_channel_select = {
         let selected_channel = selected_channel.clone();
-        Callback::from(move |id: i32| {
-            selected_channel.set(Some(id));
+        Callback::from(move |name: String| {
+            selected_channel.set(Some(name));
         })
     };
 
@@ -264,9 +265,12 @@ fn channel_list() -> Html {
         let error = error.clone();
         
         Callback::from(move |_| {
-            if let Some(id) = *selected_channel {
-                gloo::console::log!("Entering channel:", id);
-                window().location().set_href(&format!("/channel/{}", id)).unwrap();
+            if let Some(name) = (*selected_channel).clone() {
+                match LocalStorage::set("selected_channel", name.clone()) {
+                    Ok(_) => gloo::console::log!("Channel saved to local storage"),
+                    Err(_) => gloo::console::log!("Error saving to local storage:"),
+                }
+                window().location().set_href("/channel_room").unwrap();
             } else {
                 error.set("Please select a channel first".to_string());
             }
@@ -314,11 +318,11 @@ fn channel_list() -> Html {
             }}
             <div class="channel-list">
                 { for channels.iter().map(|channel| {
-                    let is_selected = *selected_channel == Some(channel.id);
-                    let channel_id = channel.id;
+                    let is_selected = *selected_channel == Some(channel.name.clone());
+                    let channel_name = channel.name.clone();
                     let on_select = {
                         let on_channel_select = on_channel_select.clone();
-                        Callback::from(move |_| on_channel_select.emit(channel_id))
+                        Callback::from(move |_| on_channel_select.emit(channel_name.clone()))
                     };
 
                     html! {
@@ -456,136 +460,136 @@ fn channel_create() -> Html {
 
 #[function_component(ChatRoom)]
 fn chat_room() -> Html {
-   let error = use_state(|| String::new());
-   let current_channel = use_state(|| None::<Channel>);
-   let message = use_state(String::new);
-   let messages = use_state(|| Vec::<String>::new());
-   
-   let location = window().location();
-   let path = location.pathname().unwrap_or_else(|_| "/".to_string());
-   let channel_id = path.split('/').last()
-       .and_then(|id| id.parse::<i32>().ok())
-       .unwrap_or(0);
-   
-   {
-       let current_channel = current_channel.clone();
-       let error = error.clone();
-       
-       use_effect_with_deps(move |_| {
-           spawn_local(async move {
-               let response = Request::get(&format!("http://localhost:8080/channel/{}", channel_id))
-                   .send()
-                   .await;
+    let error = use_state(|| String::new());
+    let current_channel = use_state(|| None::<String>);
+    let message = use_state(String::new);
+    let messages = use_state(|| Vec::<String>::new());
+        
+    {
+        let current_channel = current_channel.clone();
+        let error = error.clone();
+        
+        use_effect_with_deps(move |_| {
+            let stored_channel = LocalStorage::get("selected_channel").unwrap_or(None);
+            current_channel.set(stored_channel);
+            spawn_local(async move {
+                // let response = Request::get(&format!("http://localhost:8080/channel/{}", channel_id))
+                //     .send()
+                //     .await;
 
-               match response {
-                   Ok(resp) if resp.ok() => {
-                       match resp.json::<Channel>().await {
-                           Ok(channel_data) => current_channel.set(Some(channel_data)),
-                           Err(_) => error.set("Error loading channel".to_string()),
-                       }
-                   }
-                   Ok(_) => {
-                       error.set("Could not load channel".to_string());
-                   }
-                   Err(_) => {
-                       error.set("Network error".to_string());
-                   }
-               }
-           });
-           || ()
-       }, ());
-   }
+                // match response {
+                //     Ok(resp) if resp.ok() => {
+                //         match resp.json::<Channel>().await {
+                //             Ok(channel_data) => current_channel.set(Some(channel_data)),
+                //             Err(_) => error.set("Error loading channel".to_string()),
+                //         }
+                //     }
+                //     _ => {
+                //         error.set("Could not load channel".to_string());
+                //     }
+                // }
+            });
+            || ()
+        }, ());
+    }
 
-   let on_message_change = {
-       let message = message.clone();
-       Callback::from(move |e: Event| {
-           if let Some(input) = e.target_dyn_into::<HtmlInputElement>() {
-               message.set(input.value());
-           }
-       })
-   };
+    let on_message_change = {
+        let message = message.clone();
+        Callback::from(move |e: Event| {
+            if let Some(input) = e.target_dyn_into::<HtmlInputElement>() {
+                message.set(input.value());
+            }
+        })
+    };
 
-   let on_send = {
-       let message = message.clone();
-       let messages = messages.clone();
-       
-       Callback::from(move |_| {
-           let message_state = message.clone();
-           let messages_state = messages.clone();
-           let msg = (*message).clone();
-           
-           if !msg.is_empty() {
-               spawn_local(async move {
-                   let response = Request::post(&format!("http://localhost:8080/channel/{}/message", channel_id))
-                       .header("Content-Type", "application/json")
-                       .json(&serde_json::json!({ "content": msg.clone() }))
-                       .unwrap()
-                       .send()
-                       .await;
+    let on_send = {
+        let message = message.clone();
+        let messages = messages.clone();
+        
+        Callback::from(move |_| {
+            let message_state = message.clone();
+            let messages_state = messages.clone();
+            let msg = (*message).clone();
+            
+            if !msg.is_empty() {
+                spawn_local(async move {
+                    // let response = Request::post(&format!("http://localhost:8080/channel/{}/message", channel_id))
+                    //     .header("Content-Type", "application/json")
+                    //     .json(&serde_json::json!({ "content": msg.clone() }))
+                    //     .unwrap()
+                    //     .send()
+                    //     .await;
 
-                   if let Ok(resp) = response {
-                       if resp.ok() {
-                           let mut new_messages = (*messages_state).clone();
-                           new_messages.push(msg);
-                           messages_state.set(new_messages);
-                           message_state.set(String::new());
-                       }
-                   }
-               });
-           }
-       })
-   };
+                    // if let Ok(resp) = response {
+                    //     if resp.ok() {
+                    //         let mut new_messages = (*messages_state).clone();
+                    //         new_messages.push(msg);
+                    //         messages_state.set(new_messages);
+                    //         message_state.set(String::new());
+                    //     }
+                    // }
+                });
+            }
+        })
+    };
 
-   let on_exit = Callback::from(move |_| {
-       window().location().set_href("/channel_list").unwrap();
-   });
+    let on_exit = Callback::from(move |_| {
+        LocalStorage::delete("selected_channel");
+        window().location().set_href("/channel_list").unwrap();
+    });
 
-   match &*current_channel {
-       Some(channel) => html! {
-           <div class="chat-container">
-               <div class="chat-header">
-                   <div class="header-left">
-                       <button onclick={on_exit} class="exit-button">{"Exit"}</button>
-                       <h2 class="channel-title">
-                           {format!("Channel: {} (Owner: {})", channel.name, channel.owner)}
-                       </h2>
-                   </div>
-               </div>
-               {if !(*error).is_empty() {
-                   html! { <div class="error-message">{&*error}</div> }
-               } else {
-                   html! {}
-               }}
-               <div class="chat-messages">
-                   {for messages.iter().map(|msg| {
-                       html! {
-                           <div class="message">{msg}</div>
-                       }
-                   })}
-               </div>
-               <div class="chat-input">
-                   <input
-                       type="text"
-                       placeholder="Type a message..."
-                       value={(*message).clone()}
-                       onchange={on_message_change}
-                       class="message-input"
-                   />
-                   <button onclick={on_send} class="send-button">{"Send"}</button>
-               </div>
-           </div>
-       },
-       None => html! {
-           <div class="loading-container">
-               <h2>{"Loading channel..."}</h2>
-               {if !(*error).is_empty() {
-                   html! { <div class="error-message">{&*error}</div> }
-               } else {
-                   html! {}
-               }}
-           </div>
-       }
-   }
+    match &*current_channel {
+        Some(channel) => html! {
+            <div class="chat-container">
+                <div class="chat-header">
+                    <div class="header-left">
+                        <button onclick={on_exit} class="exit-button">{"Exit"}</button>
+                        <h2 class="channel-title">
+                            {format!(
+                                "Channel: {}",
+                                match &*current_channel {
+                                    Some(channel) => channel.clone(),
+                                    None => "None".to_string(), // Default message if no channel is selected
+                                }
+                            )}
+                        </h2>
+                    </div>
+                </div>
+                {if !(*error).is_empty() {
+                    html! { <div class="error-message">{&*error}</div> }
+                } else {
+                    html! {}
+                }}
+                <div class="chat-messages">
+                    {for messages.iter().map(|msg| {
+                        html! {
+                            <div class="message">{msg}</div>
+                        }
+                    })}
+                </div>
+                <div class="chat-input">
+                    <input
+                        type="text"
+                        placeholder="Type a message..."
+                        value={(*message).clone()}
+                        onchange={on_message_change}
+                        class="message-input"
+                    />
+                    <button onclick={on_send} class="send-button">{"Send"}</button>
+                </div>
+            </div>
+        },
+        None => html! {
+            <div class="loading-container">
+                <h2>{"Loading channel..."}</h2>
+                {if !(*error).is_empty() {
+                    html! { <div class="error-message">{&*error}</div> }
+                } else {
+                    html! {}
+                }}
+            </div>
+        }
+    }
 }
 
 #[function_component(Index)]
